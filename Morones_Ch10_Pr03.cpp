63f96efced812c34a153e1558e5be7abbf96255e
retroJacob <38300315+retroJacob@users.noreply.github.com>
2019-04-10 07:40:31 -0500

    Update Morones_Ch10_Pr03.cpp

--- Morones_Ch10_Pr03.cpp
+++ Morones_Ch10_Pr03.cpp
@@ -9,6 +9,40 @@
 
 using namespace std;
 
+//TicTacToe.h / Tic-Tac-Toe Header file  
+ 
+enum status { WIN, DRAW, CONTINUE };
+
+class ticTacToe
+{
+public:
+	void play();
+	//Function to start the play.
+
+	void displayBoard() const;
+	//Function to print the board.
+
+	bool isValidMove(int x, int y) const;
+	//Function to determine if a move is valid.
+
+	bool getXOMove(char moveSymbol);
+	//Function to get a move for a player
+
+	status gameStatus();
+	//Function to determine the current status of the game.
+
+	void reStart();
+	//Function to restart the game.
+
+	ticTacToe();
+	//Default constructor.
+	//Postcondition: Initializes the board to an empty state.
+
+private:
+	char board[3][3];
+	int noOfMoves;
+};
+
 int main()
 {
 	ticTacToe game;
